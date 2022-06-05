d18f9fe023fb3dd7ea9bd85b65d8e28a95bd9bc8
Matt <38109405+mmcguire91@users.noreply.github.com>
2022-06-05 16:42:51 -0500

    getting started

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'venturebegins';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  const checkIfWalletIsConnected = async() => {
+    try {
+      const { solana } = window;
+
+      if (solana && solana.isPhantom) {
+        console.log('Phantom wallet found');
+
+        const response = await solana.connect({ onlyIfTrusted: true});
+        console.log('Connected with Public Key:', response.publicKey.toString());
+      } else {
+        alert('Solana object not found. Please download Phantom');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
@@ -21,7 +46,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
